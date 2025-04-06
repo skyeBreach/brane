03166d2c498be2ee0b8136084c6d006ac5316090
skyeBreach <skyebenson@hotmail.com>
2025-04-06 09:12:34 +0100

    build: add cmake config/version file gen

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
+#include <version.h>
+
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello World\n";
+    std::cout << "Project Name: " << brane::PROJECT_NAME << "\n";
     return 0;
 }
 
---
+++ src/core/include/version.h
@@ -0,0 +1,23 @@
+#ifndef BRANE_VERSION_H
+#define BRANE_VERSION_H
+
+namespace brane {
+    constexpr std::string_view PROJECT_NAME = "Brane";
+    constexpr std::string_view PROJECT_DESCRIPTION = "";
+    constexpr std::string_view PROJECT_REPO = "https://github.com/skyeBreach/brane";
+
+    constexpr std::string_view AUTHOR_ORGANIZATION = "skyeBreach";
+    constexpr std::string_view AUTHOR_DOMAIN = "https://github.com/skyeBreach";
+    constexpr std::string_view AUTHOR_MAINTAINER = "skyeBreach";
+    constexpr std::string_view AUTHOR_EMAIL = "skyebreach@proton.me";
+
+    constexpr unsigned short int VERSION_MAJOR = 0;
+    constexpr unsigned short int VERSION_MINOR = 1;
+    constexpr unsigned short int VERSION_PATCH = 0;
+
+    constexpr std::string_view VERSION = "0.1.0";
+    constexpr std::string_view NAME_VERSION = "Brane v0.1.0";
+} // Namespace brane
+
+#endif // BRANE_VERSION_H
+
